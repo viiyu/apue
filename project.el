a398b06f524b562a45b6f9bf076bcf733521b9f7
xiaozhi <cyan.yui@gmail.com>
2016-05-08 11:59:33 +0800

    init scons project tree

---
+++ project.el
@@ -0,0 +1,16 @@
+;; This contains a list of projects you have visited.
+(setq project_file "/home/xiaozhi/workshop/working/apue/SConscript")
+(setq external_include "_external/apue.3e/include")
+(setq project_path (file-name-directory project_file))
+
+(ede-cpp-root-project "apue"
+                      :file project_file
+                      :include-path '( "/include" "/src" ".")
+                      :system-include-path (list "/usr/include"
+                                                 "/usr/include/gnu"
+                                                 "/usr/local/include"
+                                                 "/usr/include/c++/4.1.2"
+                                                 "/usr/include/c++"
+                                                 "/usr/include"
+                                                 (message "%s%s" project_path external_include))
+                      )
